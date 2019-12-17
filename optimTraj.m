1d77a3eb4edead4ded811fab8b8a0feb43b09d90
Zack Woodruff <jzwoodruff@u.northwestern.edu>
2019-12-17 15:56:46 -0600

    Set optimization method to sqp, turned off printing at each step of fmincon

--- optimTraj.m
+++ optimTraj.m
@@ -159,14 +159,14 @@ problem = inputValidation(problem);   %Check inputs
 problem = getDefaultOptions(problem); % Complete options struct
 
 %disp('Overwriting default options for fmincon - Zack')
-%problem.options.nlpOpt.Algorithm = 'sqp';
-problem.options.nlpOpt.Algorithm = 'interior-point';
+problem.options.nlpOpt.Algorithm = 'sqp';
+%problem.options.nlpOpt.Algorithm = 'interior-point';
 problem.options.nlpOpt.ConstraintTolerance = 1e-6;
 problem.options.nlpOpt.OptimalityTolerance = 1e-6;
 problem.options.nlpOpt.StepTolerance = 1e-6;
 problem.options.nlpOpt.MaxIter = 1000;
 problem.options.nlpOpt.Display='iter-detailed'; %'final-detailed'
-
+problem.options.nlpOpt.Display='final-detailed'; 
 % Overwrite max function evaluations - Zack
 if isfield(problem.options,'MaxFunEvals')
     %disp(['Overwriting the default MaxFunEvals. Setting to: ' num2str(problem.options.MaxFunEvals)])
